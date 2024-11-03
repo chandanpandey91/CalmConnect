85e3c2b898fe70bcef43cf0e31865bbc7829d944
Chandan Pandey <133705046+angry0batman@users.noreply.github.com>
2024-11-03 15:45:39 +0530

    all pages routes added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,6 +7,7 @@ import About from './pages/About'
 import Contact from './pages/Contact'
 import MyProfile from './pages/MyProfile'
 import MyAppointments from './pages/MyAppointments'
+import Appointment from './pages/Appointment'
 
 const App = () => {
   return (
@@ -20,6 +21,7 @@ const App = () => {
         <Route path='/contact' element={<Contact/>} />
         <Route path='/my-profile' element={<MyProfile/>} />
         <Route path='/my-appointments' element={<MyAppointments/>} />
+        <Route path='/appointment/:docId' element={<Appointment/>} />
       </Routes>
     </div>
   )

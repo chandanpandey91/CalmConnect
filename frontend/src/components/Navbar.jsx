59610a0a53273e41b03360d533bc17388bae0de9
Chandan Pandey <133705046+angry0batman@users.noreply.github.com>
2024-11-04 00:15:49 +0530

    updated

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -5,35 +5,38 @@ import { NavLink } from 'react-router-dom'
 
 const Navbar = () => {
   return (
-    <div>
-        <img src={assets.logo} alt="" />
-        <ul>
-            <NavLink>
-                <li>
+    <div className='flex items-center justify-between text-sm py-4 mb-5 border-b border-b-gray-400'>
+        <img className='w-24 cursor-pointer' src={assets.logo} alt="" />
+        <ul className='hidden md:flex items-start gap-5 font-medium'>
+            <NavLink to='/'>
+                <li className='py-1'>
                     HOME
                 </li>
-                <hr />
+                <hr className='border-none outline-none h-0.5 bg-primary w-3/5 m-auto hidden' />
             </NavLink>
-            <NavLink>
-                <li>
+            <NavLink to='/doctors'>
+                <li className='py-1'>
                     ALL DOCTORS
                 </li>
-                <hr />
+                <hr className='border-none outline-none h-0.5 bg-primary w-3/5 m-auto hidden' />
             </NavLink>
-            <NavLink>
-                <li>
+            <NavLink to='/about'>
+                <li className='py-1'>
                     ABOUT
                 </li>
-                <hr />
+                <hr className='border-none outline-none h-0.5 bg-primary w-3/5 m-auto hidden' />
             </NavLink>
-            <NavLink>
-                <li>
+            <NavLink to='/contact'>
+                <li className='py-1'>
                     CONTACT
                 </li>
-                <hr />
+                <hr className='border-none outline-none h-0.5 bg-primary w-3/5 m-auto hidden' />
             </NavLink>
             
         </ul>
+        <div className='flex items-center gap-4'>
+            <button className='bg-primary text-white px-8 py-3 rounded-full font-light hidden md:block'>Create account</button>
+        </div>
     </div>
   )
 }

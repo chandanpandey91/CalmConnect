315e95cdc266937d49d711d05a4e79eb9f0ca663
Chandan Pandey <133705046+angry0batman@users.noreply.github.com>
2024-11-05 20:41:47 +0530

    updated

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -3,7 +3,18 @@ import React from 'react'
 const Header = () => {
   return (
     <div>
-        
+        <div>
+            <p>
+                Book Appointment <br/> With Trusted Doctors
+            </p>
+            <div>
+                <img src={assets.group_profiles} alt=""/>
+            </div>
+        </div>
+
+        <div>
+
+        </div>
     </div>
   )
 }

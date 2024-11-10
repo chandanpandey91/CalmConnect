653731eac63f611b51ac5176c903ce35fa448c86
Chandan Pandey <133705046+angry0batman@users.noreply.github.com>
2024-11-10 21:12:21 +0530

    update

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,11 +3,14 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  // build: {
+  //   rollupOptions: {
+  //     external: ['react-router-dom'],
+  //   },
+  // }
   build: {
-    rollupOptions: {
-      external: ['react-router-dom'],
-    },
-  },
+    outDir: 'dist'
+  }
 });
 
 

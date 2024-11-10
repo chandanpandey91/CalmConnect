7e0ceb7f5c5c377e0d5dad548ff7d1928ca22238
Chandan Pandey <133705046+angry0batman@users.noreply.github.com>
2024-11-10 20:41:30 +0530

    updated

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -11,7 +11,7 @@ const Header = () => {
             </p>
             <div className='flex flex-col md:flex-row items-center gap-3 text-white text-sm font-light'>
                 <img className='w-28' src={assets.group_profiles} alt=""/>
-                <p>Simply browse through our extensive list of trusted doctors,<br/> schedule your appointment hassle-free</p>
+                <p>Simply browse through our extensive list of trusted doctors,<br className='hidden sm:block'/> schedule your appointment hassle-free</p>
             </div>
             <a href="">
               Book appointment <img src={assets.arrow_icon} alt=""/>

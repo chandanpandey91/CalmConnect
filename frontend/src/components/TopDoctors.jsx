10b669d85c17642d7319a5e8f9feefea194e55a1
Chandan Pandey <133705046+angry0batman@users.noreply.github.com>
2024-11-12 00:21:23 +0530

    updated

--- frontend/src/components/TopDoctors.jsx
+++ frontend/src/components/TopDoctors.jsx
@@ -3,7 +3,7 @@ import { doctors } from '../assets/assets';
 
 const TopDoctors = () => {
   return (
-    <div>
+    <div className='flex flex-col items-center gap-4 my-16 text-gray-900 md:mx-10'>
       <h1>Top Doctors to Book</h1>
       <p>Simply browse through our extensive list of trusted doctors.</p>
       <div>

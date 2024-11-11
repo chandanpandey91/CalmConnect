95b912901da534c68f5736135b54d72cb1e53810
Chandan Pandey <133705046+angry0batman@users.noreply.github.com>
2024-11-12 00:06:03 +0530

    updated

--- frontend/src/components/TopDoctors.jsx
+++ frontend/src/components/TopDoctors.jsx
@@ -3,7 +3,8 @@ import React from 'react'
 const TopDoctors = () => {
   return (
     <div>
-        
+        <h1>Top Doctors to Book</h1>
+        <p>Simply browse through our extensive list of trusted doctors.</p>
     </div>
   )
 }

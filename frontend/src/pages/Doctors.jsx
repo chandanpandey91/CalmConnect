230313a622ac6c463bbc48dba893d7653cc86d4d
Chandan Pandey <133705046+angry0batman@users.noreply.github.com>
2024-11-17 13:46:18 +0530

    updated

--- frontend/src/pages/Doctors.jsx
+++ frontend/src/pages/Doctors.jsx
@@ -1,6 +1,11 @@
 import React from 'react'
+import { useParams } from 'react-router-dom'
 
 const Doctors = () => {
+
+  const {speciality} = useParams()
+
+  console.log(speciality) // This will print the speciality parameter from the URL
   return (
     <div></div>
   )

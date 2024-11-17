78126c469bb9af54f0603345407809964682035d
Chandan Pandey <133705046+angry0batman@users.noreply.github.com>
2024-11-17 12:48:13 +0530

    updated

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -1,9 +1,21 @@
 import React from 'react'
+import { assets } from '../assets/assets'
 
 const Footer = () => {
   return (
     <div>
-        
+        {/* left */}
+        <div>
+            <img src={assets.logo} alt="" />
+        </div>
+        {/* center */}
+        <div>
+            
+        </div>
+        {/* right */}
+        <div>
+            
+        </div>
     </div>
   )
 }

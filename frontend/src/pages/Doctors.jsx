ede35c47a5c208f79f6be0f564c3bc6eedf0d004
Chandan Pandey <133705046+angry0batman@users.noreply.github.com>
2024-11-17 13:54:21 +0530

    updated

--- frontend/src/pages/Doctors.jsx
+++ frontend/src/pages/Doctors.jsx
@@ -1,13 +1,32 @@
-import React from 'react'
+import React, { useContext, useState } from 'react'
 import { useParams } from 'react-router-dom'
+import { AppContext } from '../context/AppContext'
 
 const Doctors = () => {
 
   const {speciality} = useParams()
+  const [filterDoc, setFilterDoc] = useState([])
 
-  console.log(speciality) // This will print the speciality parameter from the URL
+  const {doctors} = useContext(AppContext)
   return (
-    <div></div>
+    <div>
+      <p>Browse through the doctors specialist.</p>
+      <div>
+        <div>
+          <p>General Physician</p>
+          <p>Gynecologist</p>
+          <p>Dermatologist</p>
+          <p>Pediatricians</p>
+          <p>Neurologist</p>
+          <p>Gastroenterologist</p>
+        </div>
+        <div>
+          {
+            filterDoc.map()
+          }
+        </div>
+      </div>
+    </div>
   )
 }
 

8ca3b99ca6911fc387dbce400f3e4778e3650344
Chandan Pandey <133705046+angry0batman@users.noreply.github.com>
2024-11-17 12:51:23 +0530

    updated

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -7,14 +7,25 @@ const Footer = () => {
         {/* left */}
         <div>
             <img src={assets.logo} alt="" />
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
         </div>
         {/* center */}
         <div>
-            
+           <p>COMPANY</p> 
+           <ul>
+             <li>Home</li>
+             <li>About us</li>
+             <li>Contact us</li>
+             <li>Privacy Policy</li>
+           </ul>
         </div>
         {/* right */}
         <div>
-            
+          <p>GET IN TOUCH</p>
+          <ul>
+           <li>+91 123456789</li> 
+           <li>Email:  info@example.com</li>
+          </ul>  
         </div>
     </div>
   )

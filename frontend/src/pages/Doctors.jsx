e41e9cd0f38e2b069ddcbba735425fc4bc3e8e9b
Chandan Pandey <133705046+angry0batman@users.noreply.github.com>
2024-11-17 14:34:45 +0530

    updated

--- frontend/src/pages/Doctors.jsx
+++ frontend/src/pages/Doctors.jsx
@@ -6,7 +6,7 @@ const Doctors = () => {
 
   const {speciality} = useParams()
   const [filterDoc, setFilterDoc] = useState([])
-  const navigate = useNavigate
+  const navigate = useNavigate()
 
   const {doctors} = useContext(AppContext)
 
@@ -26,7 +26,7 @@ const Doctors = () => {
       <p className='text-gray-600'>Browse through the doctors specialist.</p>
       <div className='flex flex-col sm:flex-row items-start gap-5 mt-5'>
         <div className=' flex flex-col gap-4 text-sm text-gray-600'>
-          <p onClick={()=> speciality === 'General Physician' ? navigate('/doctors') : navigate(`/doctors/General Physician`)} className='{`w-[94vw] sm:auto pl-3 py-1.5 pr-16 border border-gray-300 rounded transition-all cursor-pointer`}'>General Physician</p>
+          <p onClick={()=> speciality === 'General physician' ? navigate('/doctors') : navigate(`/doctors/General physician`)} className='{`w-[94vw] sm:auto pl-3 py-1.5 pr-16 border border-gray-300 rounded transition-all cursor-pointer`}'>General physician</p>
           <p onClick={()=> speciality === 'Gynecologist' ? navigate('/doctors') : navigate(`/doctors/Gynecologist`)} className='{`w-[94vw] sm:auto pl-3 py-1.5 pr-16 border border-gray-300 rounded transition-all cursor-pointer`}'>Gynecologist</p>
           <p onClick={()=> speciality === 'Dermatologist' ? navigate('/doctors') : navigate(`/doctors/Dermatologist`)} className='{`w-[94vw] sm:auto pl-3 py-1.5 pr-16 border border-gray-300 rounded transition-all cursor-pointer`}'>Dermatologist</p>
           <p onClick={()=> speciality === 'Pediatricians' ? navigate('/doctors') : navigate(`/doctors/Pediatricians`)} className='{`w-[94vw] sm:auto pl-3 py-1.5 pr-16 border border-gray-300 rounded transition-all cursor-pointer`}'>Pediatricians</p>

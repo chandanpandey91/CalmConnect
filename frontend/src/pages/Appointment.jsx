7672fb5740e0858b24cbe5609d922dc87b0f0d2d
Chandan Pandey <133705046+angry0batman@users.noreply.github.com>
2024-12-28 17:31:06 +0530

    updated

--- frontend/src/pages/Appointment.jsx
+++ frontend/src/pages/Appointment.jsx
@@ -27,7 +27,7 @@ const Appointment = () => {
         </div>
         <div className='flex-1 border border-gray-400 rounded-lg p-8 py-7 bg-white mx-2 sm:mx-0 mt-[-80px] sm:mt-0'>
           {/* details of doctor */}
-          <p>{docInfo.name} <img src={assets.verified_icon} alt=""/> </p>
+          <p className='flex items-center gap-2 text-2xl font-medium text-gray-900'>{docInfo.name} <img className='w-5' src={assets.verified_icon} alt=""/> </p>
           <div>
             <p>{docInfo.degree} - {docInfo.speciality}</p>
             <button>{docInfo.experience}</button>
